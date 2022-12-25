2252448a9fdbad0c1dc8bf5230d84998736b6b36
hoanghailethe <lethehoanghai@gmail.com>
2022-12-25 18:10:25 +0700

    2. FB authentication config + 3. signup route

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
 const functions = require("firebase-functions");
 const admin = require('firebase-admin');
+const { getAuth, createUserWithEmailAndPassword } = require("firebase/auth");
 
 // // Create and deploy your first functions
 // // https://firebase.google.com/docs/functions/get-started
 
-admin.initializeApp() ;
+// for firebase authentication
+const firebaseConfig = {
+  apiKey: "AIzaSyDNyu6KsIHccqAbK1_wjzHbrt_Yc0muUfc",
+  authDomain: "socialap-73771.firebaseapp.com",
+  databaseURL: "https://socialap-73771-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "socialap-73771",
+  storageBucket: "socialap-73771.appspot.com",
+  messagingSenderId: "809928703007",
+  appId: "1:809928703007:web:b9642b9d62c52dba56e92a",
+  measurementId: "G-8GVN065EK0"
+};
 
+admin.initializeApp(firebaseConfig) ;
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+const express = require('express')
+const app = express() ;
+const db = admin.firestore() ;
+const auth = getAuth() ;
 
-exports.getPost = functions.https.onRequest((req, res ) => {
-    admin.firestore().collection('Posts').get()
+// create post route
+app.get('post', (req, res) => {
+ db.collection('Posts').get()
     .then(data => {
         let posts = []
         data.forEach((doc) => {
@@ -22,16 +35,16 @@ exports.getPost = functions.https.onRequest((req, res ) => {
         return res.json(posts);
     })
     .catch((err) => console.error(err)) ;
-})
+}) ;
 
-exports.createPost = functions.https.onRequest( (req, res) => {
+app.post('post', (req, res) => {
     let newPost = {
         body: req.body.body,
         handle: req.body.handle, 
-        createdAt : admin.firestore.Timestamp.fromDate(new Date())
+        createdAt : new Date().toISOString
     }
 
-    admin.firestore().collection('Post')
+    db.collection('Post')
     .add(newPost)
     .then((doc) => {
         res.json({message : `Post with id ${doc.id} created successfully`})
@@ -40,4 +53,61 @@ exports.createPost = functions.https.onRequest( (req, res) => {
         res.status(500).json({message: 'sth went wrong'})
         console.error(err)
     })
+});
+
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
+// exports.getPost = functions.https.onRequest((req, res ) => {
+//     admin.firestore().collection('Posts').get()
+//     .then(data => {
+//         let posts = []
+//         data.forEach((doc) => {
+//             posts.push(doc.data())
+//         })
+//         return res.json(posts);
+//     })
+//     .catch((err) => console.error(err)) ;
+// })
+
+// exports.createPost = functions.https.onRequest( (req, res) => {
+//     let newPost = {
+//         body: req.body.body,
+//         handle: req.body.handle, 
+//         createdAt : admin.firestore.Timestamp.fromDate(new Date())
+//     }
+
+//     admin.firestore().collection('Post')
+//     .add(newPost)
+//     .then((doc) => {
+//         res.json({message : `Post with id ${doc.id} created successfully`})
+//     })
+//     .catch((err) => {
+//         res.status(500).json({message: 'sth went wrong'})
+//         console.error(err)
+//     })
+// })
+
+//sign up route
+app.post('user', (req,res) => {
+    let newUser = {
+
+    }
+
+    //check existed handler
+
+    //create new user
+    createUserWithEmailAndPassword(auth, req.email, req.password)
+    .then((userCredential) => {
+
+    }) 
+    //return token + uid
+    .catch( (err) => {
+        res.status(500).json({message: "sth is wrong"});
+        console.error(err);
+    })
 })
+
+exports.api = functions.region('asia').https.onRequest(app);
